5111e85b1d5ca982df799ec5a5ec19d81149cc59
Nishanth Messi <nisanthrtts@gmail.com>
2023-05-29 21:32:57 +0530

    initial commit

--- server/server.js
+++ server/server.js
@@ -15,8 +15,9 @@ app.get('/', (req, res) => {
 app.listen(5000, () => {
   console.log("Server is running on port 5000")
   try {
-    
+    mongoose.connect(process.env.DB_URL)
+    console.log("Connected to MongoDB")
   } catch (error) {
-    
+    console.log("Something went wrong", error)
   }
 })
