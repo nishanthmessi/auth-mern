65f94e5b20a2a0acb98ba46c56ffa673343f2388
Nishanth Messi <nisanthrtts@gmail.com>
2023-06-06 12:04:12 +0530

    creating apis

--- server/server.js
+++ server/server.js
@@ -17,7 +17,7 @@ app.use(express.urlencoded({ extended: true }))
 app.use('/api/users', userRoutes)
 
 app.get('/', (req, res) => {
-  res.send('Hello World')
+  res.send('Server is up and running')
 })
 
 app.listen(process.env.port, () => {
